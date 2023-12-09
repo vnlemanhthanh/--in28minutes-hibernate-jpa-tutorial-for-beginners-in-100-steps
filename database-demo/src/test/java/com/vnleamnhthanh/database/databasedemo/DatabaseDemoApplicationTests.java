db4e5a7eeecc70356161e7637be2f4c1e0f981fb
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-10 00:02:38 +0700

    Module JDBC To JPA #1

---
+++ database-demo/src/test/java/com/vnleamnhthanh/database/databasedemo/DatabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnleamnhthanh.database.databasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
