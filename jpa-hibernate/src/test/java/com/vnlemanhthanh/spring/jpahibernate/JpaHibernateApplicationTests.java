86e9663452eee4dc93ea835ce4baf6ea432e22b9
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-11 15:52:37 +0700

    Module: JPA - Hibernate in Depth

---
+++ jpa-hibernate/src/test/java/com/vnlemanhthanh/spring/jpahibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.spring.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
