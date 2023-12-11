9b6472c28738850e4468fdffd5b4a471644779ca
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-11 10:39:19 +0700

    Spring JDBC CRUD

--- database-demo/src/main/java/com/vnleamnhthanh/database/databasedemo/DatabaseDemoApplication.java
+++ database-demo/src/main/java/com/vnleamnhthanh/database/databasedemo/DatabaseDemoApplication.java
@@ -1,5 +1,11 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
 package com.vnleamnhthanh.database.databasedemo;
 
+
+import com.vnleamnhthanh.database.databasedemo.entity.Person;
 import com.vnleamnhthanh.database.databasedemo.jdbc.PersonJdbcDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
 
 @SpringBootApplication
 public class DatabaseDemoApplication implements CommandLineRunner {
@@ -19,6 +28,12 @@ public class DatabaseDemoApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         logger.info("All users -> {}", dao.findAll());
         logger.info("User id 10001 -> {}", dao.findById(10001));
+        logger.info("Delete user id 10002. No of row deleted -> {}", dao.deleteById(10002));
+        logger.info("Inserting 10004 -> {}",
+                dao.insert(new Person(10004, "Dung", "SG", new Date())));
+        logger.info("Update user 10003 -> {}",
+                dao.update(new Person(10003, "Khai", "Singapore", new Date())));
+
     }
 
     public static void main(String[] args) {
--- database-demo/src/main/java/com/vnleamnhthanh/database/databasedemo/jdbc/PersonJdbcDao.java
+++ database-demo/src/main/java/com/vnleamnhthanh/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -10,16 +10,13 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public class PersonJdbcDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
-
-    public PersonJdbcDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     public List<Person> findAll() {
         return jdbcTemplate.query("select * from person",
@@ -31,4 +28,27 @@ public class PersonJdbcDao {
                 new Object[]{id},
                 new BeanPropertyRowMapper<>(Person.class));
     }
+
+    public int deleteById(int id) {
+        return jdbcTemplate.update("delete from person where id=?", new Object[]{id});
+    }
+
+    public int insert(Person person) {
+        return jdbcTemplate.update(
+                "insert into person (id, name, location, birth_date) "
+                + "values (?, ?, ?, ?)",
+                new Object[]{person.getId(), person.getName(), person.getLocation(),
+                        new Timestamp(person.getBirthDate().getTime())}
+        );
+    }
+
+    public int update(Person person) {
+        return jdbcTemplate.update(
+                "update person "
+                + "set name = ?, location = ?, birth_date = ? where id = ?",
+                new Object[] {person.getName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime()),
+                        person.getId()}
+        );
+    }
+
 }
