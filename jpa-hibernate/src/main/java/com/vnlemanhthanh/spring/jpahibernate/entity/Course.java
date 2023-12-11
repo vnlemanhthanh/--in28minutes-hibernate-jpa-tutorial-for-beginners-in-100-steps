e777563f196312b64850aeae54715772830d9f65
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-11 16:01:23 +0700

    Create JPA Entity Course

--- jpa-hibernate/src/main/java/com/vnlemanhthanh/spring/jpahibernate/entity/Course.java
+++ jpa-hibernate/src/main/java/com/vnlemanhthanh/spring/jpahibernate/entity/Course.java
@@ -5,7 +5,32 @@
 package com.vnlemanhthanh.spring.jpahibernate.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Course {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    protected Course() {
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
